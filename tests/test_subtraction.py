d52673f265182cf582f5203a3bc64e301afdc992
oneday2321 <oneday2321@163.com>
2024-12-22 18:30:21 +0800

    add allure

--- tests/test_subtraction.py
+++ tests/test_subtraction.py
@@ -1,23 +1,28 @@
 # -*- coding: UTF-8 -*-
 import pytest
+import allure
 
-# 执行数学运算
-def perform_operation(operation, a, b):
-    if operation == 'addition':
-        return a + b
-    elif operation == 'subtraction':
-        return a - b
-    elif operation == 'multiplication':
-        return a * b
-    elif operation == 'division':
-        if b == 0:
-            raise ValueError("Cannot divide by zero")
-        return a / b
 
-# 检查结果
-def check_result(actual_result, expected_result):
-    assert actual_result == expected_result, f"Expected {expected_result}, but got {actual_result}"
+@allure.feature("算数运算")
+class TestSubtraction:
 
-def test_subtraction(test_case):
-    result = perform_operation(test_case['Operation'], test_case['A'], test_case['B'])
-    check_result(result, test_case['ExpectedResult'])
+    def perform_operation(self,operation, a, b):
+        if operation == 'addition':
+            return a + b
+        elif operation == 'subtraction':
+            return a - b
+        elif operation == 'multiplication':
+            return a * b
+        elif operation == 'division':
+            if b == 0:
+                raise ValueError("Cannot divide by zero")
+            return a / b
+
+    # 检查结果
+    def check_result(self,actual_result, expected_result):
+        assert actual_result == expected_result, f"Expected {expected_result}, but got {actual_result}"
+
+    @allure.title('Operation: {test_case[Operation]}')
+    def test_subtraction(self,test_case):
+        result = self.perform_operation(test_case['Operation'], test_case['A'], test_case['B'])
+        self.check_result(result, test_case['ExpectedResult'])
